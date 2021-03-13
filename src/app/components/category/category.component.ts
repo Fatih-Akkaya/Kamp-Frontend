95ffc1aeb386c5bd2b7afe8d8641264d0caecc7a
Fatih-Akkaya <fatihakkaya@hotmail.com>
2021-03-14 01:42:11 +0300

    13 Mart - Category-Product Route Added

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories:Category[]=[];
+  currentCategory:Category;
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -20,5 +21,20 @@ export class CategoryComponent implements OnInit {
       this.categories=response.data;
     });
 
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory=category;
+  }
+  getCurrentCategoryClass(category:Category){
+    if(category==this.currentCategory){
+      return "list-group-item active"
+    }
+    return "list-group-item"
+  }
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }
+    return "list-group-item"
   }
 }
